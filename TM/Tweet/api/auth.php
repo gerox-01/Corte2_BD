4e866a5da5017e5df2d72a4ebbdfad5c2a9f978e
David Gerónimo Quiroga Torres <gedaquito@gmail.com>
2022-04-25 15:39:56 -0500

    Change auth

--- TM/Tweet/api/auth.php
+++ TM/Tweet/api/auth.php
@@ -13,57 +13,49 @@ $key = 'my_secret_key';
 $time = time();
 
 //Método POST para autenticar camster.com
-if($_SERVER['REQUEST_METHOD']=='POST'){
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // $credenciales = json_decode(file_get_contents("php://input"), true);
     $usuario = $_POST['usuario'];
     $contrasena = $_POST['clave'];
     $usuarioDB = ObtenerUsuarioDB($CONN, $usuario);
-    if($usuarioDB != NULL){
+    if ($usuarioDB != NULL) {
         // if($usuarioDB['clave'] == $contrasena){
-            $token = array(
-                'data' => ['usuario' => $usuario], //datos del usuario
-                'iat' => $time, //Tiempo inicio del token
-                'exp' => $time + (60*60) //Tiempo de expiración del token
-            );
-            $jwt = JWT::encode($token, $key);
-            $datos = ['token' => $jwt];
-            header("HTTP/1.1 200 OK");
-            echo json_encode($datos);
-            exit();
+        $token = array(
+            'data' => ['usuario' => $usuario], //datos del usuario
+            'iat' => $time, //Tiempo inicio del token
+            'exp' => $time + (60 * 60) //Tiempo de expiración del token
+        );
+        $jwt = JWT::encode($token, $key);
+        $datos = ['token' => $jwt];
+        header("HTTP/1.1 200 OK");
+        echo json_encode($datos);
+        exit();
         // }else{
         //     header("HTTP/1.1 401 Unauthorized");
         //     exit();
         // }
-    }else{
+    } else {
         header("HTTP/1.1 401 Unauthorized");
         exit();
     }
 }
 
 //Trae el usuario de la base de datos
-if($_SERVER['REQUEST_METHOD']=='GET'){
-    // $token = $_GET['token'];
-    $jwt = $_SERVER['HTTP_AUTHORIZATION'];
-    $key = 'my_secret_key';
-    try{
-        $decoded = JWT::decode($token, $key, array('HS256'));
-        $decoded_array = (array) $decoded;
-        $usuario = $decoded_array['usuario'];
-        $usuarioDB = ObtenerUsuarioDB($CONN, $usuario);
-        if($usuarioDB != NULL){
-            header("HTTP/1.1 200 OK");
-            echo json_encode($usuarioDB);
-            exit();
-        }else{
-            header("HTTP/1.1 401 Unauthorized");
-            exit();
-        }
-    }catch(Exception $e){
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $CONN = ConexionDB();
+
+    $token = $_GET['token'];
+    $decoded = JWT::decode($token, $key, array('HS256'));
+    $usuario = $decoded->data->usuario;
+    $usuarioDB = ObtenerUsuarioDB($CONN, $usuario);
+    if ($usuarioDB != NULL) {
+        header("HTTP/1.1 200 OK");
+        echo json_encode($usuarioDB);
+        exit();
+    } else {
         header("HTTP/1.1 401 Unauthorized");
         exit();
     }
 }
 
 header("HTTP/1.1 400 Bad Request");
-
-
